8b49f2195348269f668e9f5704d396f8182597e3
insidepixce <insidepixce@gmail.com>
2023-07-09 13:39:22 +0900

    완성. 26시간동안 뭣같았고 ... 다시보긴해야겠지... 로그인기능업데이트할거니깐....

--- app.js
+++ app.js
@@ -10,10 +10,21 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, 'public', 'image'));
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now());
+    cb(null, file.fieldname + '-' + Date.now() + '.jpeg');
   }
 });
-const upload = multer({ storage });
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype === 'image/jpeg') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  }
+});
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,11 +32,12 @@ app.use(express.static(__dirname));
 app.use('/image', express.static('public/image'));
 app.set("view engine", "ejs");
 app.use('/image', express.static(path.join(__dirname, 'public', 'image')));
-app.use(express.static('public', {
+app.use('/image', express.static(path.join(__dirname, 'public', 'image'), {
   setHeaders: function (res, path, stat) {
     res.set('Cache-Control', 'public, max-age=31536000'); // 1 year
   }
 }));
+
 
 const url = 'mongodb+srv://sparta:test@sparta.rqx1qlk.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'todoapp';
@@ -64,7 +76,7 @@ app.post('/add', async (req, res) => {
   try {
     const result = await counterCollection.findOne({ name: '게시물갯수' });
     const totalPost = result.totalPost;
-    await postCollection.insertOne({ 제목: req.body.title, 날짜: req.body.date, check: 'x', _id: totalPost });
+    await postCollection.insertOne({ 제목: req.body.title, 날짜: req.body.date, check: 'x'});
     console.log('저장완료');
     await counterCollection.updateOne({ name: "게시물갯수" }, { $inc: { totalPost: 1 } });
     const postresult = await postCollection.find().toArray();
@@ -83,12 +95,12 @@ app.get('/list', async (req, res) => {
     console.error('Error while retrieving posts:', error);
     res.status(500).send('게시물을 가져오는데 실패하였습니다.');
   }
-});
+}); 
 
 app.post('/upload', upload.single('photo'), async (req, res) => {
   try {
     const post = {
-      photo: 'https://dawnchecker.fly.dev/' + req.file.filename, // 수정된 부분
+      filename: req.file.filename,
       content: req.body.content,
       currentTime: req.body.currentTime
     };
@@ -101,6 +113,9 @@ app.post('/upload', upload.single('photo'), async (req, res) => {
     res.status(500).send('업로드에 실패하였습니다.');
   }
 });
+
+
+
 
 
 
@@ -118,7 +133,7 @@ app.post('/updateCheck/:id', async (req, res) => {
   try {
     const postId = req.params.id;
     const checkValue = req.body.check === 'on' ? 'o' : 'x';
-    await postCollection.updateOne({ _id: parseInt(postId) }, { $set: { check: checkValue } });
+    await postCollection.updateOne({ _id:  new ObjectId(postId) }, { $set: { check: checkValue } });
     console.log('체크값 업데이트 완료');
     res.redirect('/list');
   } catch (error) {
@@ -126,6 +141,7 @@ app.post('/updateCheck/:id', async (req, res) => {
     res.status(500).send('체크값 업데이트에 실패하였습니다.');
   }
 });
+
 
 app.delete('/delete/:id', async (req, res) => {
   try {
@@ -139,6 +155,10 @@ app.delete('/delete/:id', async (req, res) => {
   }
 });
 
+
+
+
+
 app.delete('/gong/delete/:id', async (req, res) => {
   try {
     const postId = req.params.id;
@@ -150,3 +170,5 @@ app.delete('/gong/delete/:id', async (req, res) => {
     res.sendStatus(500);
   }
 });
+
+
